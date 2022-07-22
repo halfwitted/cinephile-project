67b87a51a4448b6266adfddd4f9074fcf1c97286
Abhishek <abhiabi.fm@gmail.com>
2022-07-22 20:40:50 +0530

    editing urls pattern

--- appcinephile/urls.py
+++ appcinephile/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index,name='index'),
+    path('',views.index,name='index'),
     path('search',views.search,name='search'),
     path('searchquery',views.searchquery,name='searchquery'),
     path('/details/<movie_id>',views.details,name='details'),
