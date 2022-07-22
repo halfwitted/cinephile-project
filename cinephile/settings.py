dbd44545de251ab14d91fb4ff62161fb95db60dc
Abhishek <abhiabi.fm@gmail.com>
2022-07-22 06:12:16 +0530

    add static_root

--- cinephile/settings.py
+++ cinephile/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'cinephile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
